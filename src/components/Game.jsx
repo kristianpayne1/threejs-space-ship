12b76477c59882a74ec7c803ea88f238b81af963
Kristian Payne <kristianpayne1@gmail.com>
2025-04-07 19:55:36 +0100

    Add environment map

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,12 +1,28 @@
 import { TextureProvider } from "../hooks/useTextures.jsx";
 import Ship from "./Ship.jsx";
+import { Environment, OrbitControls } from "@react-three/drei";
 
 function Game() {
     return (
         <TextureProvider>
+            <OrbitControls />
             <ambientLight />
             <directionalLight />
             <Ship position={[0, 0, -20]} />
+            <Environment
+                background={true}
+                path={"./textures/environment/"}
+                backgroundIntensity={0.3}
+                backgroundRotation={[0, Math.PI / 4, 0]}
+                files={[
+                    "px.png",
+                    "nx.png",
+                    "py.png",
+                    "ny.png",
+                    "pz.png",
+                    "nz.png",
+                ]}
+            />
         </TextureProvider>
     );
 }
