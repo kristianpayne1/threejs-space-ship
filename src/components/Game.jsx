b8bbfbc6851c2057fb0db2824782d0a7db80654d
Kristian Payne <kristianpayne1@gmail.com>
2025-11-08 18:17:30 +0000

    Add collider to ship

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -5,36 +5,39 @@ import { CurrentTextureProvider } from "../hooks/useCurrentTexture.jsx";
 import SpaceParticles from "./SpaceParticles.jsx";
 import { FlightControlsProvider } from "../hooks/useFlightControls.jsx";
 import Asteroids from "./Asteroids.jsx";
+import { Physics } from "@react-three/rapier";
 
 function Game() {
     return (
-        <TextureProvider>
-            <FlightControlsProvider>
-                <CurrentTextureProvider>
-                    <Ship position={[0, 0, -20]} />
-                </CurrentTextureProvider>
-                <SpaceParticles />
-                <Asteroids />
-                <Asteroids variant={2} minScale={0.005} maxScale={0.025} />
-                <Asteroids variant={3} minScale={0.005} maxScale={0.025} />
-            </FlightControlsProvider>
-            <ambientLight />
-            <directionalLight intensity={3} position={[-10, 10, -10]} />
-            <Environment
-                background={true}
-                path={"./textures/environment/"}
-                backgroundIntensity={0.3}
-                backgroundRotation={[0, Math.PI / 4, 0]}
-                files={[
-                    "px.png",
-                    "nx.png",
-                    "py.png",
-                    "ny.png",
-                    "pz.png",
-                    "nz.png",
-                ]}
-            />
-        </TextureProvider>
+        <Physics debug={true}>
+            <TextureProvider>
+                <FlightControlsProvider>
+                    <CurrentTextureProvider>
+                        <Ship position={[0, 0, -20]} />
+                    </CurrentTextureProvider>
+                    <SpaceParticles />
+                    <Asteroids />
+                    <Asteroids variant={2} minScale={0.005} maxScale={0.025} />
+                    <Asteroids variant={3} minScale={0.005} maxScale={0.025} />
+                </FlightControlsProvider>
+                <ambientLight />
+                <directionalLight intensity={3} position={[-10, 10, -10]} />
+                <Environment
+                    background={true}
+                    path={"./textures/environment/"}
+                    backgroundIntensity={0.3}
+                    backgroundRotation={[0, Math.PI / 4, 0]}
+                    files={[
+                        "px.png",
+                        "nx.png",
+                        "py.png",
+                        "ny.png",
+                        "pz.png",
+                        "nz.png",
+                    ]}
+                />
+            </TextureProvider>
+        </Physics>
     );
 }
 
--- src/components/Ship.jsx
+++ src/components/Ship.jsx
@@ -1,3 +1,4 @@
+import { RigidBody, CuboidCollider } from "@react-three/rapier";
 import { useGLTF } from "@react-three/drei";
 import { animated } from "@react-spring/three";
 import { useRef } from "react";
@@ -5,53 +6,75 @@ import useFlightControls from "../hooks/useFlightControls.jsx";
 import Thrusters from "./Thrusters.jsx";
 import Crosshair from "./Crosshair.jsx";
 import Guns from "./Guns.jsx";
+import { Quaternion, Vector3 } from "three";
+import { useFrame } from "@react-three/fiber";
 
 function Ship({ position, ...props }) {
-    const ref = useRef(null);
-    const shipRef = useRef(null);
+    const bodyRef = useRef(null);
+    const visualRef = useRef(null);
 
     const { nodes, materials } = useGLTF("./models/Space_Ship.glb");
 
-    const [springs] = useFlightControls(ref, { position });
+    const [springs] = useFlightControls(visualRef, { position });
+
+    useFrame(() => {
+        if (!bodyRef.current || !visualRef.current) return;
+
+        const body = bodyRef.current;
+        const ship = visualRef.current;
+
+        const pos = new Vector3();
+        const quat = new Quaternion();
+        ship.getWorldPosition(pos);
+        ship.getWorldQuaternion(quat);
+
+        body.setNextKinematicTranslation(pos);
+        body.setNextKinematicRotation(quat);
+    });
 
     return (
-        <animated.group
-            ref={ref}
-            position={springs.position}
-            rotation={springs.rotation}
-            {...props}
-        >
-            <mesh
-                ref={shipRef}
-                castShadow={true}
-                receiveShadow={true}
-                geometry={nodes.SM_Ship_Fighter_03.geometry}
-                material={materials["Material.001"]}
-                rotation={[Math.PI / 2, 0, 0]}
-                scale={0.004}
-            />
-            <Thrusters
-                positions={[
-                    [-0.93, 0.4, -1.15],
-                    [0.93, 0.4, -1.15],
-                ]}
-            />
-            <Guns
-                guns={[
-                    {
-                        type: "laser_gun",
-                        position: [-1.5, 0.575, 0],
-                        rotation: [0, 0, Math.PI * -0.1],
-                    },
-                    {
-                        type: "laser_gun",
-                        position: [1.5, 0.575, 0],
-                        rotation: [0, 0, Math.PI * 0.1],
-                    },
-                ]}
-            />
-            <Crosshair />
-        </animated.group>
+        <>
+            <RigidBody ref={bodyRef} type="kinematicPosition" colliders={false}>
+                <CuboidCollider args={[1.5, 0.5, 2]} position={[0, 0.2, 0]} />
+            </RigidBody>
+
+            <animated.group
+                ref={visualRef}
+                position={springs.position}
+                rotation={springs.rotation}
+                {...props}
+            >
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.SM_Ship_Fighter_03.geometry}
+                    material={materials["Material.001"]}
+                    rotation={[Math.PI / 2, 0, 0]}
+                    scale={0.004}
+                />
+                <Thrusters
+                    positions={[
+                        [-0.93, 0.4, -1.15],
+                        [0.93, 0.4, -1.15],
+                    ]}
+                />
+                <Guns
+                    guns={[
+                        {
+                            type: "laser_gun",
+                            position: [-1.5, 0.575, 0],
+                            rotation: [0, 0, Math.PI * -0.1],
+                        },
+                        {
+                            type: "laser_gun",
+                            position: [1.5, 0.575, 0],
+                            rotation: [0, 0, Math.PI * 0.1],
+                        },
+                    ]}
+                />
+                <Crosshair />
+            </animated.group>
+        </>
     );
 }
 
